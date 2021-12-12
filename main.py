46991d56e435a21d81b9f656b974284f2de03267
dev-KC20 <dev@kitconseil.fr>
2021-12-12 15:36:40 +0100

    chore: initialise mvc folders

---
+++ main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""Manage chess tournaments after swiss rules.
+
+New attemp to nicely setup MVC alongside OOP
+"""
+
+# import constants
+from controllers.appcontroller import AppController
+
+if __name__ == "__main__":
+
+    AppController().start()
